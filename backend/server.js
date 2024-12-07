78bdc747f12dc6177210d7fb34f005fbc20b61a5
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-07 23:29:42 +0530

    connect

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 
 
@@ -17,6 +18,9 @@ app.use(cors());  //using this we access the backend from any frontend
 
 //db connectin
 connectDB();
+
+//appi endpoint
+app.use("/api/food",foodRouter);
 
 app.get("/",(req,res)=>{
 
