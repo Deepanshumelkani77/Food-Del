43b0f96f2f1fdf28615d95ddc89d8131b564ab1b
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-07 22:19:52 +0530

    make server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,25 @@
- //https://github.com/Deepanshumelkani77/Tomato.git
+import express from "express";
+import cors from "cors";
+
+
+
+//app config
+const app=express();
+const port=4000;
+app.listen(port,()=>{
+    console.log("server is running",port);
+});
+
+//middleware
+app.use(express.json()); //we send request frontend to backend
+app.use(cors());  //using this we access the backend from any frontend
+
+
+app.get("/",(req,res)=>{
+
+    res.send("api working")
+});
+
+
+//deepumelkani123:projects123@cluster0.yywca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
