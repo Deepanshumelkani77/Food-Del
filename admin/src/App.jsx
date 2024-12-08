0ec0c15f3b1eb89d40cc0b56d5b70e32e66c3878
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-08 23:14:18 +0530

    add css property for navbar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import Sidebar from "./components/sidebar/Sidebar"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <hr/>
       <div className="app-content">
-<Sidebar/>        
+<Sidebar/>       
       </div>
       
     </div>
