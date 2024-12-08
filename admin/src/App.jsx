a119f0bce8704ee301313c30ca547be9c30ecc77
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-08 22:50:52 +0530

    add navbaand sidebar component

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>
+      <Navbar/>
+      <div className="app-content">
+<Sidebar/>        
+      </div>
       
     </div>
   )
