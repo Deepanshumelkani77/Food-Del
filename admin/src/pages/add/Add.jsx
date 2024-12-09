dbd7b5bda439b99d20ead868382eabcf53ae0634
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-09 21:22:45 +0530

    add css property for add page

--- admin/src/pages/add/Add.jsx
+++ admin/src/pages/add/Add.jsx
@@ -10,8 +10,8 @@ const Add = () => {
 
 <div className="add-img-upload flex-col">
   <p>Upload Image</p>
-<label htmlFor="image"></label>
-<img src={assets.upload_area} alt="" />
+  <label htmlFor="image"></label>
+  <img src={assets.upload_area} alt="" />
 <input type="file" id='image' hidden required/>
 </div>
 
@@ -29,14 +29,14 @@ const Add = () => {
   <div className="add-category flex-col">
 <p>Product category</p>
 <select name="category" >
-  <option value="Salad"></option>
-  <option value="Rolls"></option>
-  <option value="Deserts"></option>
-  <option value="Sandwich"></option>
-  <option value="Cake"></option>
-  <option value="Pure Veg"></option>
-  <option value="pasta"></option>
-  <option value="Noodles"></option>
+  <option value="Salad">Salad</option>
+  <option value="Rolls">Rolls</option>
+  <option value="Deserts">Deserts</option>
+  <option value="Sandwich">Sandwich</option>
+  <option value="Cake">Cake</option>
+  <option value="Pure Veg">Pure Veg</option>
+  <option value="pasta">pasta</option>
+  <option value="Noodles">Noodles</option>
 </select>
   </div>
   <div className="add-price flex-col">
