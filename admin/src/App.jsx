8b4234e666f8cdd245a781ddeb44c3b73f759607
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-09 20:38:01 +0530

    set routes

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
 import Sidebar from "./components/sidebar/Sidebar"
+import {Route, Routes} from "react-router-dom"
+import Add from "./pages/add/Add.jsx"
+import List from "./pages/list/List.jsx"
+import Orders from "./pages/orders/Orders.jsx"
 
 const App = () => {
   return (
@@ -8,7 +12,13 @@ const App = () => {
       <Navbar/>
       <hr/>
       <div className="app-content">
-<Sidebar/>       
+<Sidebar/>    
+<Routes>
+  <Route path="/add" element={<Add/>}></Route>
+  <Route path="/list" element={<List/>}></Route>
+  <Route path="/orders" element={<Orders/>}></Route>
+
+  </Routes>  
       </div>
       
     </div>
