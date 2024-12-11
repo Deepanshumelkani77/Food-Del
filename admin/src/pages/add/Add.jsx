05c06ec7c4aeb87299fd69ffec7d440b85b69fb9
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-11 23:22:25 +0530

    set assits folder

--- admin/src/pages/add/Add.jsx
+++ admin/src/pages/add/Add.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import "./Add.css"
-import { assets } from '../../assets/assets'
+
 
 const Add = () => {
 
--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import "./List.css"
-
+import ExploreMenu from "../../components/exploremenu/ExploreMenu.jsx"
+import FoodDisplay from "../../components/fooddisplay/FoodDisplay.jsx"
 const List = () => {
+
+  const[category ,setCategory]=useState("All");
+
   return (
     <div>
       
+      <ExploreMenu category={category} setCategory={setCategory}/>
+<FoodDisplay category={category}/>
+
     </div>
   )
 }
