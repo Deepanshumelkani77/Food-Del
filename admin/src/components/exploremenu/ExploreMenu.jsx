43d998566f2919a8483b7ea181f7f71053008b3f
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-12 18:17:25 +0530

    remove h2 tag

--- admin/src/components/exploremenu/ExploreMenu.jsx
+++ admin/src/components/exploremenu/ExploreMenu.jsx
@@ -6,7 +6,7 @@ const ExploreMenu = ({category,setCategory}) => {
 
   return (
     <div className='explore-menu' id='explore-menu'>
-<h1>Explore  menu</h1>
+
 
 <div className="explore-menu-list">
     {menu_list.map((item,index)=>{
