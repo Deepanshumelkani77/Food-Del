5a34cc6ef557ab6fcf4d7f4e8245bed9d9e29afc
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-12 19:03:16 +0530

    fetch data in edit page

--- admin/src/pages/edit/Edit.jsx
+++ admin/src/pages/edit/Edit.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import "../add/Add.css"
+import { useParams } from 'react-router-dom';
 
 const Edit = () => {
+
+    const { id } = useParams(); // Get the dynamic ID from the URL
+    const [foodItem, setFoodItem] = useState({});
+  
+    useEffect(() => {
+      // Fetch the food item's details from the backend
+      fetch(`http://localhost:4000/foods/${id}`)
+        .then((response) => response.json())
+        .then((data) => setFoodItem(data))
+        .catch((error) => console.error('Error fetching food item:', error));
+    }, [id]);
+
+
+
+
+
   return (
     <div className='add'>
 
@@ -9,25 +26,25 @@ const Edit = () => {
 
       <div className="add-product-name ">
 <p>Product name</p>
-<input type="text" name='name'  placeholder='Type here' />
+<input type="text" name='name'  placeholder={foodItem.name} />
 </div>
 
 
 <div className="add-img-upload ">
   <p>Upload Image</p>
-<input  type="text" id='image' placeholder="Type here"  required/>
+<input  type="text" id='image' placeholder={foodItem.image}  required/>
 </div>
 
 
 <div className="add-product-discription ">
   <p>Product description</p>
-  <textarea name="discription" row='5' placeholder='write content here' required></textarea>
+  <textarea name="discription" row='5' placeholder={foodItem.description} required></textarea>
 </div>
 
 <div className="add-category-price">
   <div className="add-category ">
 <p>Product category</p>
-<select name="category" >
+<select name="category" value={foodItem.category}>
   <option value="Salad">Salad</option>
   <option value="Rolls">Rolls</option>
   <option value="Deserts">Deserts</option>
@@ -40,7 +57,7 @@ const Edit = () => {
   </div>
   <div className="add-price ">
     <p>Product price</p>
-    <input type="number" name="price" placeholder='$20'/>
+    <input type="number" name="price" placeholder={foodItem.price}/>
   </div>
 </div>
 
