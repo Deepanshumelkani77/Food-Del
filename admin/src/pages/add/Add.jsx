beb5fd1f2f4efdb803a38dc32ecb0b5bff0b98f1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2024-12-13 20:33:18 +0530

    set usenavigate

--- admin/src/pages/add/Add.jsx
+++ admin/src/pages/add/Add.jsx
@@ -1,6 +1,6 @@
-import React, { useNavigate,useState } from 'react'
+import React, { useState } from 'react'
 import "./Add.css"
-
+import {useNavigate} from 'react-router-dom'
 
 const Add = () => {
 
