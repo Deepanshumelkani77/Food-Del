c15f7187241df8bb05c5d44218fc5561399401de
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-02-23 14:43:03 +0530

    set storecontext

--- admin/src/context/StoreContext.jsx
+++ admin/src/context/StoreContext.jsx
@@ -7,10 +7,50 @@ import axios from "axios";
 const StoreContextProvider=(props)=>{
 
 
+  
+  //store current user than we use currentuser anywhere
+  const [user, setUser] = useState(Cookies.get("user") ? JSON.parse(Cookies.get("user")) : null);
+
+  //state variable for login page
+const [showLogin,setShowLogin]=useState(false)
+  
+  const login = async (email, password) => {
+      try {
+        const response = await axios.post("http://localhost:4000/admin/login", { email, password });
+        Cookies.set("token", response.data.token, { expires: 1 });
+        Cookies.set("user", JSON.stringify(response.data.user), { expires: 1 });
+        setUser(response.data.user);
+      } catch (error) {
+        alert(error.response?.data?.message || "Login failed");
+      }
+    };
+  
+    const signup = async (name, email, password) => {
+      try {
+        await axios.post("http://localhost:4000/admin/signup", { name, email, password });
+        alert("Signup successful! Please login.");
+      } catch (error) {
+        alert(error.response?.data?.message || "Signup failed");
+      }
+    };
+  
+    const logout = () => {
+      Cookies.remove("token");
+      Cookies.remove("user");
+      setUser(null);
+    };
+  
+
 
 
   const contextValue={
 
+    login,
+    signup,
+    logout,
+    user,
+    showLogin,
+    setShowLogin
 
         
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import ReactDom from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
 import StoreContextProvider from './context/StoreContext.jsx'
 
-
+//ya setup krna sa phla storecontext setup kar lo
 ReactDom.createRoot(document.getElementById('root')).render(
  <BrowserRouter>
  
