29773052b54ade95fa40522ead7b728792ae9844
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-05 14:09:16 +0530

    created review Schema

--- backend/models/Food.js
+++ backend/models/Food.js
@@ -1,7 +1,7 @@
 //database connection
 const mongoose = require("mongoose");
 const {Schema}=mongoose;
-
+const Review=require("./review.js")
 
 const foodSchema=new mongoose.Schema({
 
@@ -9,7 +9,12 @@ name:{type:String,require:true},
 image:{type:String,require:true},
 price:{type:Number,require:true},
 description:{type:String,require:true},
-category:{type:String,require:true}
+category:{type:String,require:true},
+review:[{
+            
+    type:Schema.Types.ObjectId,
+    ref:"Review"
+}]
 
 
 })
---
+++ backend/models/review.js
@@ -0,0 +1,21 @@
+//database connection
+const mongoose = require("mongoose");
+const {Schema}=mongoose;
+const User=require("./user.js")
+
+const reviewSchema=new mongoose.Schema({
+
+    comment:String,
+    created:{
+        type:Date,
+        default:Date.now()
+    },
+    author:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+
+})
+
+const Review=mongoose.model("Review",reviewSchema);
+module.exports= Review;
