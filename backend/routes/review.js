812d1ba359d164316f3c839fb8f9498afe225a44
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-08 01:46:44 +0530

    create route for deleting review

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -36,6 +36,25 @@ router.post("/:id",async(req,res)=>{
     
 })
 
+router.delete("/delete/:id",async(req,res)=>{
+
+  const { id } = req.params;
+
+  try {
+    const deletedReview = await Review.findByIdAndDelete(id);
+    if (!deletedReview) {
+      return res.status(404).json({ message: 'Review not found' });
+    }
+    res.status(200).json({ message: 'Review deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting review:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+
+
+
+})
+
 
 
 module.exports=router;
