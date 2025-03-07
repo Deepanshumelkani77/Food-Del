7d54bc380c9cf0d1c989387d6c86e75a935cd4c7
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-07 14:09:44 +0530

    try to solve bug

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -4,8 +4,14 @@ const Review =require('../models/review.js');
 const Food = require("../models/Food.js");
 
 router.post("/:id",async(req,res)=>{
+    let {id}=req.params;
+    let food=await Food.findById(id);
+    let newreview=new Review({comment:comment,author:author});
+    food.review.push(newreview);
 
 
+
+    res.status(201).send({ message: 'review added successfully' });
 
     
 })
