748fb6b7f7d57ddde2f2fe4308d472a8a63c2f43
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-08 01:28:37 +0530

    set if else case

--- frontend/src/pages/item/Item.jsx
+++ frontend/src/pages/item/Item.jsx
@@ -121,7 +121,7 @@ const handleSubmit = async (e) => {
               <div key={index} className='user-review'>
                 <p className='username'>{review.author.username || 'Anonymous'}</p>
                 <p className='comment'>{review.comment}</p>
-                <button>delete</button>
+                {user && user._id===review.author.id?<button>delete</button>:""}
               </div>
             ))
           ) : (
