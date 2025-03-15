b588897fe9e8b6f29325616aee74663567c93889
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-15 14:47:29 +0530

    make order component

--- admin/src/pages/order/Order.jsx
+++ admin/src/pages/order/Order.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import "./Order.css"
 const Order = () => {
   return (
-    <div>
-      <h1>hello</h1>
+    <div className='orders'>
+
+      <div className="order">
+        <div className="order-info"></div>
+        <div className="customer-info"></div>
+      </div>
+
+      <div className="order"></div>
+
+      <div className="order"></div>
+
     </div>
   )
 }
