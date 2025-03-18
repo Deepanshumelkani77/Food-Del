d5a40277251bdb90632b05af6ef30385d0e51e9d
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-18 13:28:00 +0530

    create a route for foods page

--- backend/routes/foods.js
+++ backend/routes/foods.js
@@ -2,33 +2,11 @@ const express=require("express");
 const router=express.Router();
 const Food = require("../models/Food.js");
 const mongoose = require("mongoose");
-
+const foodsController=require("../controller/foods.js")
 
 
 //show(item) page
-router.get("/:id", async (req, res) => {
-    try {
-      const { id } = req.params;  // Extract the id from params correctly
-      if (!mongoose.Types.ObjectId.isValid(id)) {  // Optional: check if id is a valid ObjectId
-        return res.status(400).json({ message: 'Invalid food ID' });
-      }
-  
-      //to get review
-      const foodItem = await Food.findById(id).populate({
-        path: "review",
-        populate: { path: "author" }, // Optional: to get full user details
-      });;  // Use 'id' directly
-  
-      if (!foodItem) {
-        return res.status(404).json({ message: 'Food item not found' });
-      }
-  
-      res.json(foodItem);  // Return the food item as JSON
-    } catch (error) {
-      console.error('Error fetching food item:', error);
-      res.status(500).json({ message: 'Server error', error });
-    }
-  });
+router.get("/:id", foodsController.showItem);
 
 
 
