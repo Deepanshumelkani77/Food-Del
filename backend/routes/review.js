c3c8ced4ed82efe27e6b4bccf81f5c235f0720f7
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-18 14:00:23 +0530

    controller for user

--- backend/routes/review.js
+++ backend/routes/review.js
@@ -1,7 +1,5 @@
 const express=require("express");
 const router=express.Router();
-const Review =require('../models/review.js');
-const Food = require("../models/Food.js");
 const reviewController=require("../controller/review.js");
 
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,32 +1,10 @@
 const express=require("express");
 const router=express.Router();
-const User=require("../models/user.js");
-const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
+const userController=require("../controller/user.js")
 // Signup Route
-router.post("/signup", async (req, res) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, email, password: hashedPassword });
-    await user.save();
-    res.status(201).json({ message: "User registered successfully" });
-  });
+router.post("/signup",userController.signup);
   
   // Login Route
-  router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user) return res.status(400).json({ message: "User not found" });
-  
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-  
-    const token = jwt.sign({ id: user._id }, "secret", { expiresIn: "1h" });
-    res.json({ token, user: {id:user._id, name: user.username, email: user.email } });
-    
-  });
+  router.post("/login",userController.login);
 
 module.exports=router;
