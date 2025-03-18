a75634590f1a333ac4a96854c1e23583fb900ab1
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-18 13:51:21 +0530

    set controller for order

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -41,3 +41,41 @@ module.exports.getData=async (req, res) => {
     
     
     }
+
+
+
+    module.exports.saveData2=async(req,res)=>{
+
+        const { firstname, lastname, email, street, city, state, pin_code, country, phone_no, userId } = req.body;
+    
+        try {
+          const updatedOrder = await Order.findOneAndUpdate(
+            { users:userId }, // Find order by userId
+            { firstname:firstname, lastname:lastname, email:email, street:street, city:city, state:state, pin_code:pin_code, country:country, phone_no:phone_no }, // Update fields
+            { new: true, upsert: true } // Return updated order & create if not found
+          );
+      
+          res.status(200).json({ message: "Order updated successfully", updatedOrder });
+        } catch (error) {
+          console.error("Error updating order:", error);
+          res.status(500).json({ message: "Internal server error" });
+        }
+    
+      }
+
+
+      module.exports.deleteData=async (req, res) => {
+         
+          const { id } = req.params;
+        
+          try {
+            const deletedOrder = await Order.findByIdAndDelete(id);
+            if (!deletedOrder) {
+              return res.status(404).json({ message: 'Order is not found' });
+            }
+            res.status(200).json({ message: 'Order deleted successfully' });
+          } catch (error) {
+            console.error('Error deleting food item:', error);
+            res.status(500).json({ message: 'Internal server error' });
+          }
+        }
