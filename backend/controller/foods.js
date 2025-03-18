0da37fb49c96890968ba717e0c683c4db53ae1b3
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-18 13:33:02 +0530

    controller set for crud operation

--- backend/controller/foods.js
+++ backend/controller/foods.js
@@ -36,3 +36,53 @@ module.exports.showItem=async (req, res) => {
         res.status(500).json({ message: "Error fetching data", error });
       }
     }
+
+    module.exports.addItem=async(req, res) => {
+    
+        const { name, description,category, price,image } = req.body;
+      console.log(req.body)
+    const food1=new Food({name:name,image:image,description:description,category:category,price:price})
+    food1.save();  
+    
+        res.status(201).send({ message: 'Food item added successfully' });
+      }
+
+
+
+
+      module.exports.editItem=async (req, res) => {
+          const { id } = req.params;
+          console.log(req.body)
+          const { name , description, category, price,image} = req.body;
+       
+          // Perform update logic here
+          try {
+            // Assume updateFood is a function that updates the food item in the database
+            const updatedFood = await Food.findByIdAndUpdate(
+              id, 
+              { name:name, description:description, category:category, price:price,image },
+              { new: true } // Return the updated document
+            );
+            res.status(200).json({ message: 'Food item updated successfully', updatedFood });
+          } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: 'Internal server error' });
+          }
+        }
+
+
+
+        module.exports.deleteItem=async (req, res) => {
+          const { id } = req.params;
+        
+          try {
+            const deletedFood = await Food.findByIdAndDelete(id);
+            if (!deletedFood) {
+              return res.status(404).json({ message: 'Food item not found' });
+            }
+            res.status(200).json({ message: 'Food item deleted successfully' });
+          } catch (error) {
+            console.error('Error deleting food item:', error);
+            res.status(500).json({ message: 'Internal server error' });
+          }
+        }
--- backend/routes/foods.js
+++ backend/routes/foods.js
@@ -16,59 +16,20 @@ router.get("/", foodsController.fetchItem)
 
 
 //add new item
-router.post("/", async(req, res) => {
-
-    const { name, description,category, price,image } = req.body;
-  console.log(req.body)
-const food1=new Food({name:name,image:image,description:description,category:category,price:price})
-food1.save();  
-
-    res.status(201).send({ message: 'Food item added successfully' });
-  });
+router.post("/", foodsController.addItem);
   
  
 
 
 
-  router.put('/edit/:id', async (req, res) => {
-    const { id } = req.params;
-    console.log(req.body)
-    const { name , description, category, price,image} = req.body;
- 
-    // Perform update logic here
-    try {
-      // Assume updateFood is a function that updates the food item in the database
-      const updatedFood = await Food.findByIdAndUpdate(
-        id, 
-        { name:name, description:description, category:category, price:price,image },
-        { new: true } // Return the updated document
-      );
-      res.status(200).json({ message: 'Food item updated successfully', updatedFood });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Internal server error' });
-    }
-  });
+  router.put('/edit/:id', foodsController.editItem);
 
 
 
 
 
 
-router.delete('/delete/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    const deletedFood = await Food.findByIdAndDelete(id);
-    if (!deletedFood) {
-      return res.status(404).json({ message: 'Food item not found' });
-    }
-    res.status(200).json({ message: 'Food item deleted successfully' });
-  } catch (error) {
-    console.error('Error deleting food item:', error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+router.delete('/delete/:id', foodsController.deleteItem);
 
 
 
