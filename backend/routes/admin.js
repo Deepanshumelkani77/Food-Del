5b10eabbdcbca14bc565e079f09db9942688f076
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-03-18 13:37:40 +0530

    controller routes done for admin login

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -4,33 +4,14 @@ const Admin=require("../models/admin.js");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-
+const adminController=require("../controller/admin.js")
 
 // Signup Route
-router.post("/signup", async (req, res) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const admin = new Admin({ username, email, password: hashedPassword });
-    await admin.save();
-    res.status(201).json({ message: "User registered successfully" });
-  });
+router.post("/signup",adminController.signup );
 
   
   // Login Route
-  router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
-    const admin = await Admin.findOne({ email });
-
-    if (!admin) return res.status(400).json({ message: "User not found" });
-  
-    const isMatch = await bcrypt.compare(password, admin.password);
-    if (!isMatch) return res.status(400).json({ message: "Invalid credentials" });
-  
-    const token = jwt.sign({ id: admin._id }, "secret", { expiresIn: "1h" });
-    res.json({ token, admin: {id:admin._id, name: admin.username, email: admin.email } });
-
-
-  });
+  router.post("/login",adminController.login);
 
 
 
