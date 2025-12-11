715663f3a38db18ed204ee173d5297bc296652d2
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-12-11 21:20:20 +0530

    done

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
-const User=require("./user.js")
+const User=require("./User.js")
 
 const cartSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
