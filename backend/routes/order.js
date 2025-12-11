1dd3583ef3eb84ec0142b87f61eaebc70a02cf92
Deepanshumelkani77 <deepumelkani123@gmail.com>
2025-12-11 21:33:46 +0530

    all done

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -6,7 +6,7 @@ const {
   placeOrder, 
   getUserOrders, 
   getOrderById ,getAllOrders,updateOrderStatus
-} = require('../controller/OrderController');
+} = require('../controller/orderController');
 
 
 
